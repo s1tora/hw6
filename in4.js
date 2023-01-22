19131cab5f0b25b50d718a569d2f5dd1d478dbd4
s1tora <samirkhodzha@mail.ru>
2023-01-22 16:46:11 +0600

    test3

--- in4.js
+++ in4.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) {
 window.onload = () => {
     detect();
   };
@@ -41,3 +42,37 @@ window.onload = () => {
       render();
     })();
   }
+  
+  var video = document.querySelector("#videoElement");
+            var stopVideo = document.querySelector("#stop");
+            var startVideo = document.querySelector("#start");
+ 
+ 
+            stopVideo.addEventListener("click", stop, false);
+            startVideo.addEventListener("click", startWebCam, false);
+ 
+ 
+            function startWebCam() {
+                if (navigator.mediaDevices.getUserMedia) {
+                    navigator.mediaDevices.getUserMedia({video: true})
+                            .then(function (stream) {
+                                video.srcObject = stream;
+                            })
+                            .catch(function (err0r) {
+                                console.log("Something went wrong!");
+                            });
+                }
+            }
+ 
+            function stop(e) {
+                var stream = video.srcObject;
+                var tracks = stream.getTracks();
+ 
+                for (var i = 0; i < tracks.length; i++) {
+                    var track = tracks[i];
+                    track.stop();
+                }
+ 
+                video.srcObject = null;
+            }
+        })
