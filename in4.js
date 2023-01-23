ba2785c9a8e7bee9626f106c9e4d35f31aec88fe
s1tora <samirkhodzha@mail.ru>
2023-01-23 16:28:34 +0600

    test4

--- in4.js
+++ in4.js
@@ -1,38 +1,39 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-   
-    var video = document.querySelector("#videoElement");
-    var stopVideo = document.querySelector("#stop");
-    var startVideo = document.querySelector("#start");
+ 
+  var video = document.querySelector("#videoElement");
+  var stopVideo = document.querySelector("#stop");
+  var startVideo = document.querySelector("#start");
 
 
-    stopVideo.addEventListener("click", stop, false);
-    startVideo.addEventListener("click", startWebCam, false);
+  stopVideo.addEventListener("click", stop, false);
+  startVideo.addEventListener("click", startWebCam, false);
 
 
-    function startWebCam() {
-        if (navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({video: true})
-                    .then(function (stream) {
-                        video.srcObject = stream;
-                    })
-                    .catch(function (err0r) {
-                        console.log("Something went wrong!");
-                    });
-        }
-    }
+  function startWebCam() {
+      if (navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({video: true})
+                  .then(function (stream) {
+                      video.srcObject = stream;
+                  })
+                  .catch(function (err0r) {
+                      console.log("Something went wrong!");
+                  });
+      }
+  }
 
-    function stop(e) {
-        var stream = video.srcObject;
-        var tracks = stream.getTracks();
+  function stop(e) {
+      var stream = video.srcObject;
+      var tracks = stream.getTracks();
 
-        for (var i = 0; i < tracks.length; i++) {
-            var track = tracks[i];
-            track.stop();
-        }
+      for (var i = 0; i < tracks.length; i++) {
+          var track = tracks[i];
+          track.stop();
+      }
 
-        video.srcObject = null;
-    }
-    window.onload = () => {
+      video.srcObject = null;
+  }
+
+  window.onload = () => {
     detect();
   };
   
@@ -75,4 +76,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
       render();
     })();
   }
-})
+}) 
